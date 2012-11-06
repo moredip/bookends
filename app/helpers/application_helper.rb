70a72a6a400cc3c84e094a0828633d6e32ca89a2
matt <matthewcopeland1@gmail.com>
2012-11-06 16:46:35 -0600

    Moved company naming to its own helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-  def client_nda
-    true
-  end
-
-  def client_name
-    return "Client"
-  end
-
-  def client_url
-  end
 end
---
+++ app/helpers/company_naming_helper.rb
@@ -0,0 +1,24 @@
+module CompanyNamingHelper
+  def company_name
+    return "ThoughtWorks"
+  end
+
+  def company_full_name
+    return "ThoughtWorks Inc"
+  end
+
+  def company_url
+    return "http://thoughtworks.com"
+  end
+
+  def client_nda
+    true
+  end
+
+  def client_name
+    return "Client"
+  end
+
+  def client_url
+  end
+end
