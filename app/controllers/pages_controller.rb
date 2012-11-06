c2a348350361b1c207c0dccfb3a30a37b23cc058
matt <matthewcopeland1@gmail.com>
2012-11-06 16:09:09 -0600

    Footer navigation and nab link tweaks.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,26 @@
 class PagesController < ApplicationController
+
   def rwd
+    @pdf_download = true
   end
 
   def knowledge_share
+    @pdf_download = true
   end
 
   def continuous_delivery
+    @pdf_download = true
   end
 
   def design_collaboration
+    @pdf_download = true
   end
 
   def walking_skeleton
+    @pdf_download = true
   end
 
   def architecture
+    @pdf_download = true
   end
 end
