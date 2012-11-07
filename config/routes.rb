6b01caa1d7a8216aa6166f3491f7f439c6e04fa5
matt <matthewcopeland1@gmail.com>
2012-11-07 10:23:43 -0600

    Updated Copy.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Bookends::Application.routes.draw do
   root :to => "home#index"
 
   get "pages/rwd", :as => "rwd"
-  get "pages/knowledge_share", :as => "knowledge_share"
   get "pages/continuous_delivery", :as => "continous_delivery"
   get "pages/design_collaboration", :as => "design_collaboration"
   get "pages/walking_skeleton", :as => "walking_skeleton"
